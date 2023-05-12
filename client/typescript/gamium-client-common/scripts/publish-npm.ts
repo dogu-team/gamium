377915d8a7d03990ff11a8b0204c581d7e5f52b4
Byoungwook Park <qkrquddnr99@gmail.com>
2023-05-12 13:27:32 +0900

    🚧 gamium client ts npmignore

--- client/typescript/gamium-client-common/scripts/publish-npm.ts
+++
@@ -1,9 +0,0 @@
-import { NpmPublisher } from '@dogu-dev-private/publish-package';
-
-(async (): Promise<void> => {
-  const publisher = new NpmPublisher();
-  await publisher.publish();
-})().catch((error) => {
-  console.error(error);
-  process.exit(1);
-});

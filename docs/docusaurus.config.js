ce99325e081cb417dc5f032e8ecb032fc2aba034
ikalli <andoune0719@gmail.com>
2023-05-15 20:53:01 +0900

    🔧 Docs deployment cname, branch config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -19,6 +19,7 @@ const config = {
   organizationName: 'dogu-team', // Usually your GitHub org/user name.
   projectName: 'gamium', // Usually your repo name.
   trailingSlash: false,
+  deploymentBranch: 'gh-docs',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want

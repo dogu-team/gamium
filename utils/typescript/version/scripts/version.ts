a3957aa3dec7ed37715c6476ebcaeaf97f06bcb0
Byoungwook Park <qkrquddnr99@gmail.com>
2023-12-13 19:48:59 +0900

    fix: update gamium unity pacakge version script

--- utils/typescript/version/scripts/version.ts
+++ utils/typescript/version/scripts/version.ts
@@ -4,6 +4,8 @@ import fs from 'fs';
 
 const ROOT_PATH = path.resolve(__dirname, '..', '..', '..', '..');
 const UNITY_SERVER_VERSION_FILE_PATH = path.resolve(ROOT_PATH, 'engine/unity/Runtime/Private/Network/Server.cs');
+const UNITY_SERVER_PACKAGE_VERSION_FILE_PATH = path.resolve(ROOT_PATH, 'engine/unity/package.json');
+const UNITY_SERVER_DOCS_GITREF_VERSION_FILE_PATH = path.resolve(ROOT_PATH, 'docs/docs/docs/engine/unity/project-configuration.mdx');
 const PYTHON_CLIENT_VERSION_FILE_PATH = path.resolve(ROOT_PATH, 'client/python/gamium/pyproject.toml');
 const TYPESCRIPT_CLIENT_VERSION_FILE_PATH = path.resolve(ROOT_PATH, 'client/typescript/gamium/package.json');
 
@@ -19,6 +21,35 @@ function changeUnityServerVersion(version: string) {
   const newContents = contents.replace(oldVersion, version);
   fs.writeFileSync(UNITY_SERVER_VERSION_FILE_PATH, newContents);
   console.log('Updated Unity Server version');
+}
+
+function changeUnityServerPackageVersion(version: string) {
+  const contents = fs.readFileSync(UNITY_SERVER_PACKAGE_VERSION_FILE_PATH, 'utf-8');
+
+  const regex = /"version": "(\d+\.\d+\.\d+)",/;
+  const match = contents.match(regex);
+  if (!match) {
+    throw new Error('Could not find version in Unity Server Package');
+  }
+  const oldVersion = match[1];
+  const newContents = contents.replace(oldVersion, version);
+  fs.writeFileSync(UNITY_SERVER_PACKAGE_VERSION_FILE_PATH, newContents);
+  console.log('Updated Unity Server Package version');
+}
+
+function changeUnityServerDocsPackageVersion(version: string) {
+  const contents = fs.readFileSync(UNITY_SERVER_DOCS_GITREF_VERSION_FILE_PATH, 'utf-8');
+
+  const regex = /"com.dogu.gamium.engine.unity": ".*",/;
+  const match = contents.match(regex);
+  if (!match) {
+    throw new Error('Could not find version in Unity Server Package');
+  }
+  const oldVersion = match[0];
+  const newVersionLine = `"com.dogu.gamium.engine.unity": "https://github.com/dogu-team/gamium.git?path=/engine/unity#${version}",`;
+  const newContents = contents.replace(oldVersion, newVersionLine);
+  fs.writeFileSync(UNITY_SERVER_DOCS_GITREF_VERSION_FILE_PATH, newContents);
+  console.log('Updated Unity Server docs version');
 }
 
 function changePythonClientVersion(version: string) {
@@ -57,6 +88,8 @@ program
   .action((version) => {
     console.log(`Upgrading to ${version}`);
     changeUnityServerVersion(version);
+    changeUnityServerPackageVersion(version);
+    changeUnityServerDocsPackageVersion(version);
     changePythonClientVersion(version);
     changeTypescriptClientVersion(version);
   });
